51ec4014a3c7f5ba669a816f316144f6bbb003e2
Andrew-Carlson <acarlson2398@gmail.com>
2022-09-29 13:29:46 -0400

    linear  regression completed

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,5 +7,5 @@ mechacar_table <- read.csv("MechaCar_mpg.csv", header = TRUE, check.names = F, s
 # perform a multiple linear regression by passing in all  columns into lm() function with mpg being the dependent variable
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar_table)
 
-# use the summmary function to find  p-value and the r-squared value for the linear regression model
+# use the summmary function to find  p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar_table))
