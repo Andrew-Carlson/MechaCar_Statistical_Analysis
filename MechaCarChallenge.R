98242d1d18562ede6ff70362882752f85f2f543b
Andrew-Carlson <acarlson2398@gmail.com>
2022-09-29 18:30:53 -0400

    created t-tests for psi of suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -21,3 +21,23 @@ total_summary <- suspension_table %>% summarize(Mean=mean(suspension_table$PSI),
 
 # create  summary statistics for each lot using the group_by() functions
 lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# use the t.test() function on suspension_table to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
+
+# Note level of significance is 0.05
+
+t.test(suspension_table$PSI, mu = 1500)
+
+# Ho: mu = 1500
+# Ha: mu <> 1500 (<>: not equal to)
+# p-value is 0.06028 which is > 0.05, therefore we fail to reject the null hypothesis, and the alternate hypothesis is true.
+
+# Perform a t-test to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+#for Lot1:
+t.test(subset(suspension_table$PSI, suspension_table$Manufacturing_Lot == "Lot1"), mu =1500)
+
+#for Lot2
+t.test(subset(suspension_table$PSI, suspension_table$Manufacturing_Lot == "Lot2"), mu =1500)
+
+#for Lot3
+t.test(subset(suspension_table$PSI, suspension_table$Manufacturing_Lot == "Lot3"), mu =1500)
